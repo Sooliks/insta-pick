fc05455aefd0d36535d66ce9ec29fd467cb37437
Sooliks <evgenij.terexov.2000@mail.ru>
2024-01-03 17:05:04 +0300

    multi languages ready

--- src/contexts/ConfigContextProvider.tsx
+++ src/contexts/ConfigContextProvider.tsx
@@ -2,7 +2,7 @@
 import React, {createContext, useContext, useEffect, useState} from 'react';
 import {ConfigType} from "@/types/config";
 import {LanguageType} from "@/types/language";
-import {useRouter} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 
 
 type ConfigContextProviderProps = {
@@ -20,11 +20,16 @@ const defaultConfig: ConfigType = {
 }
 const ConfigContextProvider: React.FC<ConfigContextProviderProps> = ({children, lang}) => {
     const router = useRouter();
+    const pathname = usePathname();
     const [config,setConfig] = useState<ConfigType>({
         ...defaultConfig, currentLanguage: lang
     });
     useEffect(()=>{
-        router.replace(config.currentLanguage)
+        let newPath = pathname;
+        const searchValue = '/' + newPath.split('/')[1];
+        const replaceValue = '/' + config.currentLanguage;
+        newPath = newPath.replace(searchValue, replaceValue)
+        router.replace(newPath)
     },[config])
     return (
         <ConfigContext.Provider value={{config,setConfig}}>{children}</ConfigContext.Provider>
