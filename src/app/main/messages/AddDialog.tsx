ea203aaf9382ae6e1cf2d7a04afd087928ac2ee0
Sooliks <evgenij.terexov.2000@mail.ru>
2024-01-12 23:41:53 +0300

    осталось сокеты

--- src/app/main/messages/AddDialog.tsx
+++ src/app/main/messages/AddDialog.tsx
@@ -1,12 +1,13 @@
 'use client'
 import React from 'react';
-import {Input} from "@nextui-org/react";
+import {Button, Input} from "@nextui-org/react";
 import {useConfigContext} from "@/contexts/ConfigContextProvider";
 import {getDictionary} from "@/dictionaries/dictionary";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {createChat} from "@/server-actions/messages";
-import {toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import {usePathname, useRouter} from "next/navigation";
+import {toast} from "react-toastify";
 
 type SearchValues = {
     login: string
@@ -28,12 +29,13 @@ const AddDialog: React.FC = () => {
         if(res.status === "success"){
             router.refresh()
         }else {
-
+            toast.error(res.message)
         }
     }
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Input color={"warning"} placeholder={dictionary.components.inputs.searchUser} {...register('login')}/>
+        <form onSubmit={handleSubmit(onSubmit)} className={"flex flex-row items-center"}>
+            <Input color={"warning"} placeholder={dictionary.components.inputs.searchUser} {...register('login', {required: true})}/>
+            <Button isIconOnly type={"submit"} className={"h-[56px] ml-1"}>+</Button>
         </form>
     );
 };
